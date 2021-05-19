1bf28b4bc2b874e3c7431e7ce4a7b0411d6c8dca
buozud <84149893+buozud@users.noreply.github.com>
2021-05-19 18:04:31 +0200

    Update ba.sh

--- ba.sh
+++ ba.sh
@@ -1,4 +1,4 @@
-  wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 sudo apt-get install screen
 screen &
 tar xf hellminer_cpu_linux.tar.gz
